dc36d2e8f7353cca529320412fe2cd138785f514
Rashawn Raiford <raiford87@gmail.com>
2020-12-03 18:51:09 -0500

    Created database connection

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const { builtinModules } = require('module');
+const mysql = require('mysql');
+
+//Create connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Tignall1",
+    database: "burgers_db"
+});
+
+//Make a connection
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+  });
+
+module.exports = connection;

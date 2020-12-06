1257f007b1a92c93db30291f6a0745fecbb59aec
Rashawn Raiford <raiford87@gmail.com>
2020-12-05 22:35:16 -0500

    Fixed issue with updating burger in orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,25 @@
 const connection = require('./connection');
 
+function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
 const orm = {
     selectAll: function(table, cb){
         connection.query('SELECT * FROM ??', [table], (err, result) => {
@@ -21,8 +41,8 @@ const orm = {
         })
     },
     updateOne: function(table, updateObject, condition, cb){
-        const query = 'UPDATE ?? SET ? WHERE ' + condition
-        connection.query(query, [table, updateObject], (err, result) => {
+        const query = "UPDATE ?? SET " + objToSql(updateObject) + " WHERE " + condition
+        connection.query(query, [table, condition], (err, result) => {
             if(err){
                 throw err;
             }
